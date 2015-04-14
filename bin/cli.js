aad9e4c41e38a7a60a2e09a98c169db322d66197
Paul C Pederson <ppederson@esri.com>
2015-04-13 22:17:08 -0700

    log error if stderr is used with not other arguments

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 var stderr = require('../')
-
 var argv = process.argv.slice(2)
-stderr(argv.shift(), argv)
+var command = argv.shift()
+
+if (command) {
+  stderr(command, argv)
+} else {
+  console.error('stderr: You must pass a command to stderr')
+}
