153721e93389cc4072472e5223d6f867e8fd5fdc
Paul C Pederson <ppederson@esri.com>
2015-04-13 18:04:51 -0700

    add error handler for spawned process, fail gracefully if there is an error with notifier

--- index.js
+++ index.js
@@ -2,18 +2,33 @@ var notifier = require('node-notifier')
 var spawn = require('child_process').spawn
 var util = require('util')
 
-var errify = function (command, args, options) {
+function notify (command, args, data) {
+  try {
+    return notifier.notify({
+      'title': util.format('Error running %s %s', command, args.join(' ')),
+      'message': data.toString('utf8')
+    })
+  } catch (err) {
+    return false
+  }
+}
+
+var stderr = function (command, args, options) {
   options = options || {}
   options.stdio = [process.stdin, process.stdout, 'pipe']
 
   var child = spawn(command, args, options)
-  child.stderr.on('data', function (data) {
-    notifier.notify({
-      'title': util.format('Error running %s %s', command, args.join(' ')),
-      'message': data.toString('utf8')
-    })
-    process.stderr.write(data)
+
+  child.on('error', function (err) {
+    process.stderr.write(err)
+    notify(command, args, err)
   })
+
+  child.stderr.on('data', function (data) {
+    process.stderr.write(data)
+    notify(command, args, data)
+  })
+
 }
 
-module.exports = errify
+module.exports = stderr
