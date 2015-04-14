a88cba88350522515add22ff829273d2a39a42fe
Paul C Pederson <ppederson@esri.com>
2015-04-13 18:12:49 -0700

    stub out tests

---
+++ test/cli.js
@@ -0,0 +1,25 @@
+var stderr = require('../')
+var rimraf = require('rimraf')
+var path = require('path')
+var test = require('tape')
+var fs = require('fs')
+
+test('should correctly write to stdout', function (t) {
+  t.equal()
+  t.end()
+})
+
+test('should correctly write to stderr', function (t) {
+  t.equal()
+  t.end()
+})
+
+test('should correctly report errors', function (t) {
+  t.equal()
+  t.end()
+})
+
+test('should preserve all unix redirection', function (t) {
+  t.equal()
+  t.end()
+})
